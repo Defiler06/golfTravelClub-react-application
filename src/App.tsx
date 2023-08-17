950f059610c8212fe432ce98f6e604e0fbc3e7e8
Nogai Artem <mr.yjufq@bk.ru>
2023-08-18 00:45:18 +0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
                 <div className='container'>
                     <div className='paragraph'>
                         <div className='logo'></div>
-                        <p className='main_title'>WELCOME</p>
+                        <p className='main_title'>WELCOME TO</p>
                         <p className='main_title underline'>GOLF TRAVELERS CLUB!</p>
                         <div className='about_us'>
                             <h2 className='title_about'>About us</h2>
@@ -32,57 +32,65 @@ function App() {
                 </div>
             </div>
             <div className='our_app'>
-                <div className='our_app_title'>
-                    <p>Our application will help such people. Now golfers can easily create</p>
-                    <p><span
-                        className='green_text'>a LIFETIME COLLECTION OF THEIR COUNTRIES AND GOLF CLUBS</span> they
-                        have played in.</p>
+                <div className='container'>
+                    <div className='our_app_title'>
+                        <p>Our application will help such people. Now golfers can easily create</p>
+                        <p><span
+                            className='green_text'>a LIFETIME COLLECTION OF THEIR COUNTRIES AND GOLF CLUBS</span> they
+                            have played in.</p>
+                    </div>
                 </div>
             </div>
             <div className='background'>
-                    <div className='phone_section'>
-                        <div className='phone_container'>
-                            <div className='title_our_app'>
-                                <div className='our_app_subtitle'>
-                                    <p>The reward will be your personal</p>
-                                    <p className='green_text'>CERTIFICATE OF ACHIEVEMENTS</p>
-                                    <p>which you can send to your friends, business partners.</p>
-                                    <p>You can print it out and hang it in</p>
-                                    <p>a beautiful frame at home or in the office.</p>
-                                    <p>Your family, your friends will be proud of you!</p>
-                                    <p>And your children will take an example from you!</p>
-                                    <div className='good_luck'>
-                                        <p className='green_text'>Good luck</p>
-                                        <p className='green_text'>and enjoy your golf travels!</p>
-                                    </div>
-                                </div>
-                                <div className='qr_codes'>
-                                    <Qr title='IOS'/>
-                                    <Qr title='ANDROID'/>
+                <div className='phone_section'>
+                    <div className='phone_container'>
+                        <div className='title_our_app'>
+                            <div className='our_app_subtitle'>
+                                <p>The reward will be your personal</p>
+                                <p className='green_text'>CERTIFICATE OF ACHIEVEMENTS</p>
+                                <p>which you can send to your friends, business partners.</p>
+                                <p>You can print it out and hang it in</p>
+                                <p>a beautiful frame at home or in the office.</p>
+                                <p>Your family, your friends will be proud of you!</p>
+                                <p>And your children will take an example from you!</p>
+                                <div className='good_luck'>
+                                    <p className='green_text'>Good luck</p>
+                                    <p className='green_text'>and enjoy your golf travels!</p>
                                 </div>
                             </div>
-                            <div className='phone'></div>
+                            <div className='qr_codes'>
+                                <Qr title='IOS'/>
+                                <Qr title='ANDROID'/>
+                            </div>
                         </div>
+                        <div className='phone'></div>
                     </div>
+                </div>
             </div>
             <div className='statistic_section'>
-                <div className='statistic_title'>
-                    <p className='white_text'>App users statistics create a rating of the most</p>
-                    <p className='white_text'>popular golf clubs and hotels in the world</p>
+                <div className='container'>
+                    <div className='statistic_title'>
+                        <p className='white_text'>App users statistics create a rating of the most</p>
+                        <p className='white_text'>popular golf clubs and hotels in the world</p>
+                    </div>
                 </div>
             </div>
-            <h2 className='cooperation'>Interested in cooperation:</h2>
-            <div className='cards_section'>
-                <div className='cards'>
-                    <Card linkA4='https://www.google.com/' linkA5='https://www.google.com/' title='FOR GOLF CLUB'/>
-                    <Card linkA4='https://www.google.com/' linkA5='https://www.google.com/' title='FOR GOLF HOTELS'/>
-                </div>
-                <div className='support'>
-                    <p className='size'>ANY QUESTIONS?</p>
-                    <p><a href="mailto:INFO@GOLFTRAVELCLUB.APP" className='green_text size'>INFO@GOLFTRAVELCLUB.APP</a>
-                    </p>
-                    <p><a href="mailto:SUPPORT@GOLFTRAVELCLUB.APP"
-                          className='green_text size'>SUPPORT@GOLFTRAVELCLUB.APP</a></p>
+            <div className='container'>
+                <h2 className='cooperation'>Interested in cooperation:</h2>
+                <div className='cards_section'>
+                    <div className='cards'>
+                        <Card linkA4='https://www.google.com/' linkA5='https://www.google.com/' title='FOR GOLF CLUB'/>
+                        <Card linkA4='https://www.google.com/' linkA5='https://www.google.com/'
+                              title='FOR GOLF HOTELS'/>
+                    </div>
+                    <div className='support'>
+                        <p className='size'>ANY QUESTIONS?</p>
+                        <p><a href="mailto:INFO@GOLFTRAVELCLUB.APP"
+                              className='green_text size'>INFO@GOLFTRAVELCLUB.APP</a>
+                        </p>
+                        <p><a href="mailto:SUPPORT@GOLFTRAVELCLUB.APP"
+                              className='green_text size'>SUPPORT@GOLFTRAVELCLUB.APP</a></p>
+                    </div>
                 </div>
             </div>
         </>
